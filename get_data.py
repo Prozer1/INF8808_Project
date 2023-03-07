aa8caa120f801d8f0f34608c0337a248afe5e5c0
Prozer1 <dev.prozer@gmail.com>
2023-03-06 19:20:19 -0500

    ADD: Q1 filtered dataset

--- get_data.py
+++ get_data.py
@@ -7,3 +7,8 @@ def get_data_from_file(file_name):
 
     sheetX = xls.parse(0)
     return sheetX
+
+def question_1_data():
+    goal_data = get_data_from_file("./datasets/cr7_goals.xlsx")
+    filtered_data = goal_data.filter(['Date','Comp','Équipe', 'Partie du corps', 'Distance', 'Minute'], axis=1)
+    return filtered_data
