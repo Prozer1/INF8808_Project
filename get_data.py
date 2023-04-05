f11f53cfb2a67e2e5d1ef23eb65106303535f858
Prozer1 <dev.prozer@gmail.com>
2023-04-05 13:53:08 -0400

    ADD: Question 9 data getter

--- get_data.py
+++ get_data.py
@@ -12,3 +12,21 @@ def question_1_data():
     goal_data = get_data_from_file("./datasets/cr7_goals.xlsx")
     filtered_data = goal_data.filter(['Date','Comp','Équipe', 'Partie du corps', 'Distance', 'Minute'], axis=1)
     return filtered_data
+
+def question_9_data():
+    """This is the function to get the data and filters it to answer the question 9.
+
+    Returns:
+        dataframe: Pandas DF with Date, Type and Count as columns
+    """
+    all_goals = get_data_from_file("./datasets/all_goals.xlsx")
+    filtered_data = all_goals.filter(['Date','Type'], axis=1)
+    for count, date in enumerate(filtered_data.Date):
+        date = date.split('/')
+        if len(date) == 1:
+            date = date[0].split('-')
+        filtered_data.Date[count] = '20'+date[2]
+    filtered_data['Count'] = filtered_data.groupby(['Date', 'Type'])['Type'].transform('count')
+    filtered_data = filtered_data.dropna(subset=['Type'])
+    filtered_data = filtered_data.drop_duplicates()
+    return filtered_data
